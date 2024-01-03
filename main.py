be47e774acc9ecf61e0adee68710fdcd64137068
saurav-sabu <saurav.sabu9@gmail.com>
2024-01-03 13:34:31 +0530

    logger added

---
+++ main.py
@@ -0,0 +1,4 @@
+from Wine_Quality_ML_Project import logger
+
+
+logger.info("hello, this is my first project")
--- template.py
+++ template.py
@@ -4,7 +4,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO,format="[%(asctime)s]:%(message)s:")
 
-project_name = "Wine-Quality-ML-Project"
+project_name = "Wine_Quality_ML_Project"
 
 list_of_files = [
     f"src/{project_name}/__init__.py",
